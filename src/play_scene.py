1a7593377dbdfd468a0b09da0d0b76b053670dae
Ithan Jimenez Spencer <ithanjs@gmail.com>
2021-03-11 21:53:55 -0600

    modulación torretas

--- src/play_scene.py
+++ src/play_scene.py
@@ -1,6 +1,9 @@
 from Scene import Scene
 from enemy import Enemy
 from Turret import Turret
+from Basic_Turret import Basic_Turret
+from Heavy_Turret import Heavy_Turret
+from LongRange_Turret import LongRange_Turret
 from mapcontrol import Map
 import pygame
 import asyncio
@@ -11,7 +14,7 @@ class PlayScene(Scene):
         self.app = app
         self.screen = app.screen
         self.test = Enemy(app, 400, 400)
-        self.turret = Turret(500, 315)
+        self.turrets = [Basic_Turret(525, 469), Heavy_Turret(304, 247), LongRange_Turret(525, 247)]
         self.gamemap = Map(app)
         self.leveltodraw = None
         self.level = 1
@@ -24,18 +27,21 @@ class PlayScene(Scene):
             self.gamemap.start("assets/images/lvl1.png")
 
     def process_events(self, event):
-        if event.type == pygame.KEYDOWN:
-            self.turret.fire(self.test.currentpos.x, self.test.currentpos.y, 15, 15)
+        '''if event.type == pygame.KEYDOWN:
+            self.turret.fire(self.test.currentpos.x, self.test.currentpos.y, 15, 15)'''
+        pass
 
     def update(self):
         self.test.update()
-        self.turret.update()
+        for turret in self.turrets:
+            turret.update()
 
     def draw(self):
         self.screen.fill((255,255,255))
         self.gamemap.draw(self.gamemap.rect)
         self.test.draw()
-        self.turret.draw(self.screen)
+        for turret in self.turrets:
+            turret.draw(self.screen)
 
     def exit(self):
         print('Termina:', self.name)
