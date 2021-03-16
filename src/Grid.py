fb7519722ad09a2e00b029a0e14493d1d41990fe
Ithan Jimenez Spencer <ithanjs@gmail.com>
2021-03-15 20:44:02 -0600

    Grid

---
+++ src/Grid.py
@@ -0,0 +1,31 @@
+from Cell import Cell 
+
+class Grid:
+    def __init__(self):
+        self.grid = []
+        tam = 55
+        posX = 0
+        posY = 0
+        color = (255, 0, 0)
+        for y in range (11):
+            if color == (255, 0, 0):
+                color = (0, 255, 0)
+            elif color == (0, 255, 0):
+                color = (0, 0, 255)
+            elif color == (0, 0, 255):
+                color = (255, 0, 0)
+            for x in range(18):
+                id = (x, y)
+                posX = x * tam
+                posY = y * tam
+                self.grid.append(Cell(posX, posY, color, tam, id))
+                if color == (255, 0, 0):
+                    color = (0, 255, 0)
+                elif color == (0, 255, 0):
+                    color = (0, 0, 255)
+                elif color == (0, 0, 255):
+                    color = (255, 0, 0)
+
+    def draw(self, screen):
+        for cell in self.grid:
+            cell.draw(screen)
