8777595e590c4e339ce04d77bcc407033480c946
Marco Herrera <herreragdl@hotmail.com>
2021-03-15 21:12:14 -0600

    Se creó el Enemy pool y se hace un respaldo para intentar hacer un merge con la versión más actualizada de develop.

--- src/Enemy_Pool.py
+++ src/Enemy_Pool.py
@@ -5,19 +5,30 @@ from BasicEnemy import BasicEnemy
 import random
 
 class Enemy_Pool():
+    def __init__(self, app, poolSize, deactiveX, deactiveY):
+        self.app = app
+        self.pool = []
+        self.size = poolSize
+        self.originX = deactiveX
+        self.originY =deactiveY
 
-def __init__(self, app, poolSize, spawnX, spawnY):
-    self.app == app
-    self.pool = []
-    self.size = poolSize
-    self.fill_pool(self.size)
+    def fill_pool(self):
+        for i in range(self.size):
+            self.enemy = 3#random.randint(0, 4)
+            if(self.enemy == 1):
+                self.pool.append(BasicEnemy(self.app, self.originX, self.originY))
+            elif(self.enemy == 2):
+                self.pool.append(FastEnemy(self.app, self.originX, self.originY))
+            elif(self.enemy == 3):
+                self.pool.append(TankEnemy(self.app, self.originX, self.originY))
 
-def fill_pool(self, size):
-    for i in range(size):
-        enemy = random.randint(0, 4)
-        if(enemy == 1):
-            self.pool.append(BasicEnemy(app, spawnX, spawnY))
-        elif(enemy == 2):
-            self.pool.append(FastEnemy(app, spawnX, spawnY))
-        elif(enemy == 3):
-            self.pool.append(FastEnemy(app, spawnX, spawnY))
+    def spawn_enem(self, x, y):
+        
+
+    def draw(self):
+        for enemy in self.pool:
+            enemy.draw()
+
+    def update(self):
+        for enemy in self.pool:
+            enemy.update()
