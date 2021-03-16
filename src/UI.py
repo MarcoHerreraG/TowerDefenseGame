504ce0a25eed0f8dff875a2376d66ae4141cc296
Ithan Jimenez Spencer <ithanjs@gmail.com>
2021-03-15 21:14:39 -0600

    UI en proceso 2.0

--- src/UI.py
+++ src/UI.py
@@ -1,9 +1,14 @@
+import pygame
+
 class UI:
     def __init__(self):
         self.turretsSpawn = []
+        self.mousePos = (0, 0)
+
+    
 
     def update(self):
-        pass
+        self.mousePos = pygame.mouse.get_pos()
     
     def draw(self):
         pass
