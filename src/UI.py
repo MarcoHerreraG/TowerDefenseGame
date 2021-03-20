76811e0346806befdb57ed887dc8c452ab434b1e
Marco Herrera <herreragdl@hotmail.com>
2021-03-20 16:34:42 -0600

    Se añadió el primer intento de cambio de oleada y texto de tiempo restante.

--- src/UI.py
+++ src/UI.py
@@ -2,10 +2,11 @@ from Turret import Turret
 from Basic_Turret import Basic_Turret
 from Heavy_Turret import Heavy_Turret
 from LongRange_Turret import LongRange_Turret
+from Enemy_Pool import Enemy_Pool
 import pygame
 
 class UI:
-    def __init__(self, grid, turrets, nexus):
+    def __init__(self, grid, turrets, nexus, app):
         self.turretsSpawn = [(1, 6), (1, 7), (3, 4), (3, 6), (3, 7), (3, 8), (4, 4), (5, 4), (7, 5), (7, 6), (9, 2), (9, 4), (9, 6), (9, 7), (9, 8), (10, 4), (11, 3), (11, 5), (11, 7), (11, 9), (12, 9), (13, 4), (13, 5), (13, 6), (13, 7), (13, 8), (13, 9), (14, 9), (15, 2), (15, 6), (15, 7)] 
         self.mousePos = (0, 0)
         self.grid = grid
@@ -15,6 +16,16 @@ class UI:
         self.type3 = False #Long
         self.wallet = 300
         self.health = nexus.health
+        self.app = app
+        self.screen = app.screen
+        self.last = pygame.time.get_ticks()
+        self.title = app.font.render("Money: " + str(self.wallet), True, (255,255,255))
+        self.title_rect = self.title.get_rect()
+        self.title_rect.center = (app.width//2, app.height//2)
+        self.counter = app.font.render("Tiempo hasta la siguiente oleada: ", True, (255,255,255))
+        self.counter_rect = self.counter.get_rect()
+        self.counter_rect.center = (app.width//2, app.height+400)
+        self.cooldown = 1000
 
     def spawnTurret(self, cell):
         for spawn in self.turretsSpawn:
@@ -58,9 +69,16 @@ class UI:
                     self.spawnTurret(cell)
                     self.changeTurretType(cell)
 
-    def update(self):
+    def update(self, enem):
         self.mousePos = pygame.mouse.get_pos()
         self.getLeftClick()
+        self.title = self.app.font.render("Money: " + str(self.wallet), True, (255,255,255))
+        print("si entra al if")
+        self.now = pygame.time.get_ticks()
+        self.counter = self.app.font.render("Tiempo hasta la siguiente oleada: " + str(self.cooldown - (self.now - self.last)), True, (255,255,255))
+        self.last = pygame.time.get_ticks()
+
+
     
     def draw(self, screen):
         for cell in self.grid.grid:
@@ -79,3 +97,5 @@ class UI:
                 if self.type3 == True:
                     cell.color = (255, 255, 255)
                 cell.draw(screen)
+        self.screen.blit(self.title, self.title_rect)
+        self.screen.blit(self.counter, self.counter_rect)
